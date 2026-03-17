3ba128d37172fda0d3e8f279dd0549ad0d6921c2
sunilpawar24 <sunilpawar@gmail.com>
2026-03-17 20:10:14 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -51,7 +51,7 @@ const App = () => {
   };
 
   return (
-    <section className="min-h-screen bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 flex flex-col lg:flex-row">
+    <section className="min-h-screen bg-linear-to-r from-indigo-500 via-purple-500 to-pink-500 flex flex-col lg:flex-row">
 
       {/* FORM */}
       <div className="lg:w-1/2 w-full flex items-center justify-center p-6">
